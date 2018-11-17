118915bff05dcf9683cac8af5f4395f9dc01952a
kevin Martínez <kevinccbsg@gmail.com>
2018-11-17 13:24:56 +0100

    App tdd

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import Header from '../components/Header';
+import Button from '../components/Button';
 
-const App = () => (
-  <Header>
-    Create your cool notes
-  </Header>
-);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'home',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ route: 'app' });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header>
+          Create your cool notes
+        </Header>
+        <main>
+          <Button onClick={this.handleClick}>Access</Button>
+        </main>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/containers/App.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+import App from './App';
+
+const props = {};
+
+describe('<App />', () => {
+  
+  const shallowWrapper = shallow(<App />);
+  const mountWrapper = mount(<App />);
+
+  it('<Button /> match snapshot', () => {
+    const tree = renderer.create(<App {...props} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  console.log(shallowWrapper.debug());
+  console.log(mountWrapper.debug());
+
+  it('must render Header component' , () => {
+    expect(shallowWrapper.find('Header').exists()).toEqual(true);
+  });
+
+  it('must render Button component' , () => {
+    expect(shallowWrapper.find('Button').exists()).toEqual(true);
+  });
+
+  it('must have state property route with "home" value', () => {
+    expect(shallowWrapper.state().route).toEqual('home');
+  });
+
+  it('Button click change state property route to "app" value', () => {
+    shallowWrapper.find('Button').props().onClick();
+    expect(shallowWrapper.state().route).toEqual('app');
+  });
+});
+
