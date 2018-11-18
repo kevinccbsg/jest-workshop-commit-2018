66b31684517d7796ce3415fa6d2677dd16803411
kevin Martínez <kevinccbsg@gmail.com>
2018-11-19 00:42:39 +0100

    remove respect sound

--- server/respect.js
+++
@@ -1,9 +0,0 @@
-const player = require('play-sound')({});
-
-const play = () => {
-  player.play(__dirname + '/respect.mp3', function(err){
-    if (err) console.log('Error', err);
-  });
-}
-
-module.exports = play;
